47b43dc9c420052db8b249dbefd5cd0a3c5a3e60
Nicholas Otieno <otienonicholas02@gmail.com>
2023-12-05 19:28:37 +0000

    update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,9 @@
-
-export default function getListStudents(){
-	 return [
+/**
+ * Retrieves a list of students.
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
