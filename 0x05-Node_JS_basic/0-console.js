ed9db2f4f11536621c00be2d75781ae276d00164
Nicholas Otieno <otienonicholas02@gmail.com>
2024-01-30 22:31:14 +0300

    0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Nicholas Otieno <https://github.com/otienonicholas02>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
